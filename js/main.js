acd08dbcd26481b3e095ab227cd3efab8cf2f9b1
MRTM4M <msri777gamer@gmail.com>
2026-02-23 21:58:47 +0200

    some main polishing for the service section

--- js/main.js
+++ js/main.js
@@ -17,3 +17,98 @@ document.addEventListener('click', (e) => {
         menu.classList.remove('show-menu');
     }
 });
+
+
+if (window.matchMedia("(pointer: coarse) , (pointer: fine) , (hover: hover)").matches) {
+
+    const cards = document.querySelectorAll(".serv-card");
+
+    cards.forEach(card => {
+
+        card.addEventListener("click", (e) => {
+
+            e.stopPropagation();
+
+            
+            if (card.classList.contains("active-card")) {
+                card.classList.remove("active-card");
+            } else {
+                cards.forEach(c => c.classList.remove("active-card"));
+                card.classList.add("active-card");
+            }
+
+            
+            card.classList.add("active-card");
+        });
+
+    });
+    cards.forEach(card => {
+
+        card.addEventListener("mouseenter", (e) => {
+
+            e.stopPropagation();
+
+            
+            if (card.classList.contains("active-card")) {
+                card.classList.remove("active-card");
+            } else {
+                cards.forEach(c => c.classList.remove("active-card"));
+                card.classList.add("active-card");
+            }
+
+            
+            card.classList.add("active-card");
+        });
+
+    });
+
+    window.addEventListener("click", () => {
+        setTimeout(() => {
+            cards.forEach(c => c.classList.remove("active-card"));
+        }, 50);
+    });
+
+    cards.forEach(card => {
+        card.addEventListener("mouseleave", () => {
+            card.classList.remove("active-card");
+        });
+    });
+
+    window.addEventListener("scroll", () => {
+        setTimeout(() => {
+            cards.forEach(c => c.classList.remove("active-card"));
+        }, 50);
+    });
+
+}
+
+
+if (window.matchMedia("(pointer: fine) and (hover: hover) and (min-width: 552px)").matches) {
+    const cards = document.querySelectorAll(".serv-card");
+
+    cards.forEach(card => {
+        card.addEventListener("mousemove", (e) => {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            const centerX = rect.width / 2;
+            const centerY = rect.height / 2;
+
+            const rotateX = -(y - centerY) / 25;
+            const rotateY = (x - centerX) / 25;
+
+            
+            const activeTranslate = card.classList.contains("active-card") ? "translate3d(0, -8px, 15px)" : "translate3d(0,0,0)";
+            card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) ${activeTranslate}`;
+        });
+
+        card.addEventListener("mouseleave", () => {
+            
+            if (card.classList.contains("active-card")) {
+                card.style.transform = "translate3d(0, -8px, 15px)";
+            } else {
+                card.style.transform = "rotateX(0) rotateY(0) translate3d(0,0,0)";
+            }
+        });
+    });
+}
